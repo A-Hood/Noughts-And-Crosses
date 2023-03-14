688915fa55a6ed2d21389ce03c328c8bf464e73a
A-Hood <17hood.a@thestudio-liverpool.net>
2023-03-14 15:24:32 +0000

    Combines three ifs into one if

--- BlipBloop/XTurn.cpp
+++ BlipBloop/XTurn.cpp
@@ -9,6 +9,8 @@ std::string* XTurn(std::string* ptrField)
 	int column_in_array = 0;
 	int row = 0;
 
+	/* ======= ROW SELECTS ======= */
+
 	std::cout << "\n\nWhat row do you want to put the X in? (1, 2, 3): ";
 	std::cin >> row;
 
@@ -16,17 +18,13 @@ std::string* XTurn(std::string* ptrField)
 	{
 		if (row == 1)
 		{
-			if (!(ptrField[0] == "   "))
+			if (!(ptrField[0] == "   ") && !(ptrField[1] == "   ") && !(ptrField[2] == "   ")) // IF ROW 1 HAS NO SPACE
+
 			{
-				if (!(ptrField[1] == "   "))
-				{
-					if (!(ptrField[2] == "   "))
-					{
-						std::cout << "\n\nTurn invalid!\n\n";
-					}
-				}
+				std::cout << "\n\nTurn invalid!\n\n";
 			}
-			if (ptrField[0] == "   " || ptrField[1] == "   " || ptrField[2] == "   ")
+
+			if (ptrField[0] == "   " || ptrField[1] == "   " || ptrField[2] == "   ") // IF ANY OF ROW 1 HAS SPACE
 			{
 				column_in_array = 0;
 				turnValid = true;
@@ -36,17 +34,12 @@ std::string* XTurn(std::string* ptrField)
 
 		else if (row == 2)
 		{
-			if (!(ptrField[3] == "   "))
+			if (!(ptrField[3] == "   ") && !(ptrField[4] == "   ") && !(ptrField[5] == "   ")) // IF ROW 2 HAS NO SPACE
 			{
-				if (!(ptrField[4] == "   "))
-				{
-					if (!(ptrField[5] == "   "))
-					{
-						std::cout << "\n\nTurn invalid!\n\n";
-					}
-				}
+				std::cout << "\n\nTurn invalid!\n\n";
 			}
-			if (ptrField[3] == "   " || ptrField[4] == "   " || ptrField[5] == "   ")
+
+			if (ptrField[3] == "   " || ptrField[4] == "   " || ptrField[5] == "   ") // IF ANY OF ROW 2 HAS SPACE
 			{
 				column_in_array = 3;
 				turnValid = true;
@@ -56,17 +49,12 @@ std::string* XTurn(std::string* ptrField)
 
 		else if (row == 3)
 		{
-			if (!(ptrField[6] == "   "))
+			if (!(ptrField[6] == "   ") && !(ptrField[7] == "   ") && !(ptrField[8] == "   ")) // IF ROW 3 HAS NO SPACE
 			{
-				if (!(ptrField[7] == "   "))
-				{
-					if (!(ptrField[8] == "   "))
-					{
-						std::cout << "\n\nTurn invalid!\n\n";
-					}
-				}
+				std::cout << "\n\nTurn invalid!\n\n";
 			}
-			if (ptrField[6] == "   " || ptrField[7] == "   " || ptrField[8] == "   ")
+
+			if (ptrField[6] == "   " || ptrField[7] == "   " || ptrField[8] == "   ") // IF ROW 3 HAS ANY SPACE
 			{
 				column_in_array = 6;
 				turnValid = true;
@@ -81,56 +69,58 @@ std::string* XTurn(std::string* ptrField)
 	std::cout << "\n\n";
 	turnValid = false;
 
+	/* ======= COLUMN SELECTS ======= */
+
 	std::cout << "What column do you want to put the X in? (1, 2, 3): ";
 	std::cin >> column;
 
 	while (!(turnValid))
 	{
-		if (column == 1)
+		if (column == 1) // IF COLUMN SELECTED = 1
 		{
-			if (ptrField[column_in_array + 0] == " X ")
+			if (ptrField[column_in_array + 0] == " X ") // IF SPOT IS TAKEN
 			{
 				std::cout << "\n\nInvalid spot as it is taken!\n\n";
 			}
-			else if (ptrField[column_in_array + 0] == " O ")
+			else if (ptrField[column_in_array + 0] == " O ") // IF SPOT IS TAKEN
 			{
 				std::cout << "\n\nInvalid spot as it is taken!\n\n";
 			}
-			else if (ptrField[column_in_array + 0] == "   ")
+			else if (ptrField[column_in_array + 0] == "   ") // IF SPOT ISN'T TAKEN
 			{
 				ptrField[column_in_array + 0] = " X ";
 				return ptrField;
 			}
 		}
 
-		else if (column == 2)
+		else if (column == 2) // IF COLUMN SELECTED = 2
 		{
 			if (ptrField[column_in_array + 1] == " X ")
 			{
-				std::cout << "\n\nInvalid spot as it is taken!\n\n";
+				std::cout << "\n\nInvalid spot as it is taken!\n\n"; // IF SPOT IS TAKEN
 			}
 			else if (ptrField[column_in_array + 1] == " O ")
 			{
-				std::cout << "\n\nInvalid spot as it is taken!\n\n";
+				std::cout << "\n\nInvalid spot as it is taken!\n\n"; // IF SPOT IS TAKEN
 			}
-			else if (ptrField[column_in_array + 1] == "   ")
+			else if (ptrField[column_in_array + 1] == "   ")// IF SPOT ISN'T TAKEN
 			{
 				ptrField[column_in_array + 1] = " X ";
 				return ptrField;
 			}
 		}
 
-		else if (column == 3)
+		else if (column == 3) // IF COLUMN SELECTED = 3
 		{
-			if (ptrField[column_in_array + 2] == " X ")
+			if (ptrField[column_in_array + 2] == " X ") // IF SPOT IS TAKEN
 			{
 				std::cout << "\n\nInvalid spot as it is taken!\n\n";
 			}
-			else if (ptrField[column_in_array + 2] == " O ")
+			else if (ptrField[column_in_array + 2] == " O ") // IF SPOT IS TAKEN
 			{
 				std::cout << "\n\nInvalid spot as it is taken!\n\n";
 			}
-			else if (ptrField[column_in_array + 2] == "   ")
+			else if (ptrField[column_in_array + 2] == "   ") // IF SPOT ISN'T TAKEN
 			{
 				ptrField[column_in_array + 2] = " X ";
 				return ptrField;
